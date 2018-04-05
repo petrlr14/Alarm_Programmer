730fe5bb06a6649554c04c95c3e0e352997c9e02
petrlr14 <00057616@uca.edu.sv>
2018-04-04 21:42:01 -0600

    first commit

---
+++ app/src/main/java/com/pedrogomez00057616/pdm/alarmprogrammer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.pedrogomez00057616.pdm.alarmprogrammer;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
